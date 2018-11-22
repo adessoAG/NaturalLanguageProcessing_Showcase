71fc9c2845b992877b0d7c7aac3574d10a9d0176
Sascha Feldmann <sascha.feldmann@adesso.de>
2018-11-22 20:33:47 +0100

    Changed StanfordCoreNlpAdapter to use english models. Added travis configuration

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# NaturalLanguageProcessing_Showcase
-Spring Boot web application to demonstrate Natural Language Processing capabilities.
+[travis-image]: https://travis-ci.org/sasfeld-thesis/question-generator.svg?branch=master
+[travis-url]: https://travis-ci.org/sasfeld-thesis/question-generator
+
+# Natural Language Processing Showcase  
+
+A spring boot web application to demonstrate Natural Language Processing (NLP) capabilities in Java.
+
+##  Testing system
+
+There's a testing system provided by heroku where the most recent application is deployed continously.
+
+[Try it out](https://adesso-nlp-showcase.herokuapp.com/)
+
+## NLP adapters
+
+Beside the showcase, the idea behind the project is also to offer a stable API to run NLP tasks. The underlying adapters are designed to use different sub systems to solve the NLP tasks.
+
+### Stanford Core NLP library
+
+Currently, there's only one supported Natural Language Processing (NLP) adapter which uses the Stanford Core NLP library.
+And it only supports German raw texts right now.
+
+The implementation is provided [here](src/main/java/de/adesso/nlpshowcase/nlp/external/adapter/StanfordCoreNlpAdapter).
+
+## Local development
+
+It's a gradle project shipped with the gradle wrapper (gradlew). 
+
+To build the project, run: 
+
+```bash
+gradlew build
+```
+
+To start the web application locally, run:
+
+```bash
+gradlew bootrun
+```
+
+Run all tests:
+
+```bash
+gradlew check
+```
+
+
--- src/test/java/de/adesso/nlpshowcase/nlp/service/NlpServiceTest.java
+++ src/test/java/de/adesso/nlpshowcase/nlp/service/NlpServiceTest.java
@@ -22,10 +22,33 @@ public class NlpServiceTest {
     @Autowired
     private NlpService nlpService;
 
+//    @Test
+//    public void annotate_shouldAnnotateGermanRawText() throws Exception {
+//        // given
+//        final String givenRawText = "Berlin ist die Hauptstadt von Deutschland.";
+//
+//        // when
+//        NlpResult nlpResult = nlpService.annotate(givenRawText);
+//
+//        // then
+//        assertThat(nlpResult).isNotNull();
+//        assertThat(nlpResult.getRawText()).isEqualTo(givenRawText);
+//        assertThat(nlpResult.getAnnotatedSentences().size()).isEqualTo(1);
+//
+//        AnnotatedSentences firstSentence = nlpResult.getAnnotatedSentences().get(0);
+//        assertThat(firstSentence.getAnnotatedWords().size()).isGreaterThan(6);
+//
+//        AnnotatedWord firstWord = firstSentence.getAnnotatedWords().get(0);
+//        assertThat(firstWord.getWord()).isEqualTo("Berlin");
+//        assertThat(firstWord.getPartOfSpeechTag()).isEqualTo("NE");
+//        assertThat(firstWord.getNamedEntityRecognitionTag()).isEqualTo("LOCATION");
+//    }
+//
+
     @Test
-    public void annotate_shouldAnnotateGermanRawText() throws Exception {
+    public void annotate_shouldAnnotateEnglishRawText() throws Exception {
         // given
-        final String givenRawText = "Berlin ist die Hauptstadt von Deutschland.";
+        final String givenRawText = "Berlin is the capital of Germany.";
 
         // when
         NlpResult nlpResult = nlpService.annotate(givenRawText);
@@ -40,7 +63,7 @@ public class NlpServiceTest {
 
         AnnotatedWord firstWord = firstSentence.getAnnotatedWords().get(0);
         assertThat(firstWord.getWord()).isEqualTo("Berlin");
-        assertThat(firstWord.getPartOfSpeechTag()).isEqualTo("NE");
-        assertThat(firstWord.getNamedEntityRecognitionTag()).isEqualTo("LOCATION");
+        assertThat(firstWord.getPartOfSpeechTag()).isEqualTo("NNP");
+        assertThat(firstWord.getNamedEntityRecognitionTag()).isEqualTo("CITY");
     }
 }
